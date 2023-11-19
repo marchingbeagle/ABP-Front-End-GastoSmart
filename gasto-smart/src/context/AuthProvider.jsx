fd58a51c88953c63175c0673e82103e2bfcd6e0e
Erik Schneider <erik.schneider.pacheco@gmail.com>
2023-11-19 16:31:15 -0300

    AuthProvider context adicionado para login e signup

---
+++ gasto-smart/src/context/AuthProvider.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [logado, setLogado] = useState(false);
+
+  const signUp = (email, senha) => {
+    if (email && senha) {
+      setLogado(true);
+    }
+  };
+
+  const signIn = (email, senha) => {
+    if (email && senha) {
+      setLogado(true);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ logado, signUp, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
